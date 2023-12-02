0b03003cafd9b34902ebffd1c62a696d37ebf4a3
Vibhatia <bhatiavikas2001@gmail.com>
2023-12-02 21:21:00 +0530

    Added frontend with server.js

---
+++ README.md
@@ -0,0 +1,47 @@
+
+# Real Time Code Editor
+
+The goal was to build a full-stack real-time code editor, which will
+serve as a collaboration tool in a variety of use cases using Expressjs
+and Reactjs.
+
+
+
+
+
+
+
+## Installation
+
+Install Code-Editor with npm
+
+```bash
+  git clone https://github.com/Vibhatia/Code-Editor
+  cd Code-Editor
+```
+
+To run the project in your dev environment
+
+```bash
+  npm run server:dev
+```
+
+## Screenshots
+
+### Login
+
+[![Home.png](https://i.postimg.cc/XqpVYq8M/Home.png)](https://postimg.cc/4HRC8Jqw)
+
+### Home Page
+
+[![Editor.png](https://i.postimg.cc/1zdL8yDj/Editor.png)](https://postimg.cc/Mcys42P7)
+
+### Two Coders Collab
+
+[![Twocollab.png](https://i.postimg.cc/rFhgVQ1h/Twocollab.png)](https://postimg.cc/9r7y1PNT)
+
+### Four Coders Collab
+
+[![Fourcollab.png](https://i.postimg.cc/9MjY8S2f/Fourcollab.png)](https://postimg.cc/TyCbhtp8)
+
+
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+import toast from "react-hot-toast";
+import { v4 as uuidV4 } from "uuid";
+import { useNavigate } from "react-router-dom";
+
+const Home = () => {
+  const navigate = useNavigate();
+  const [roomId, setRoomId] = useState("");
+  const [username, setUserName] = useState("");
+
+  const createNewRoom = (e) => {
+    e.preventDefault();
+    const id = uuidV4();
+    setRoomId(id);
+    toast.success("Created a new room");
+  };
+
+  const joinRoom = (e) => {
+    if (!roomId || !username) {
+      toast.error("ROOM ID & User-name are Required");
+      return;
+    }
+    // Redirect
+    navigate(`/editor/${roomId}`, {
+      state: {
+        username,
+      },
+    });
+  };
+
+  const handleInputEnter = (e) => {
+    if (e.code === "Enter") joinRoom();
+  };
+
+  return (
+    <div className="homePageWrapper">
+      <div className="formWrapper">
+        <img
+          className="homePageLogo"
+          src="/code-sync.png"
+          alt="code-sync-logo"
+        />
+        <h4 className="mainLable">Paste invitation ROOM ID</h4>
+        <div className="inputGroup">
+          <input
+            type="text"
+            className="inputBox"
+            placeholder="ROOM ID"
+            onChange={(e) => setRoomId(e.target.value)}
+            value={roomId}
+          />
+          <input
+            type="text"
+            className="inputBox"
+            placeholder="USERNAME"
+            onChange={(e) => setUserName(e.target.value)}
+            value={username}
+          
+          />
+          <button className="btn joinBtn" onClick={joinRoom}>
+            Join
+          </button>
+          <span className="createInfo">
+            If you don't have an invite create &nbsp;
+            <a onClick={createNewRoom} href="" className="createNewBtn">
+              new room
+            </a>
+          </span>
+        </div>
+      </div>
+      <footer>
+        <h4>
+          Build With 💜 by &nbsp;
+          <a href="https://linktr.ee/Vibhatia">Vikas Bhatia</a>
+        </h4>
+      </footer>
+    </div>
+  );
+};
+
+export default Home;
